1e626cc336baf2dafd06498e936ede58d528b547
emmanuel-martinez-dev <emmamartinzwc@gmail.com>
2025-03-27 11:52:14 -0300

    separa archivos

--- app/thoughts/[slug]/page.tsx
+++ app/thoughts/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import { promises as fs } from 'fs'
-import path from 'path'
 import cn from 'clsx'
 export const runtime = 'edge';
+
 export default async function Page(props: {
   params: Promise<{
     slug: string
@@ -22,28 +21,13 @@ export default async function Page(props: {
   )
 }
 
-export async function generateStaticParams() {
-  const articles = await fs.readdir(
-    path.join(process.cwd(), 'app', 'thoughts', '_articles')
-  )
-
-  return articles
-    .filter((name) => name.endsWith('.mdx'))
-    .map((name) => ({
-      params: {
-        slug: name.replace(/\.mdx$/, ''),
-      },
-    }))
-}
-
 export async function generateMetadata(props: {
   params: Promise<{
     slug: string
   }>
 }) {
   const params = await props.params
-  const metadata = (await import('../_articles/' + `${params.slug}.mdx`))
-    .metadata
+  const metadata = (await import('../_articles/' + `${params.slug}.mdx`)).metadata
   return {
     title: metadata.title,
     description: metadata.description,
